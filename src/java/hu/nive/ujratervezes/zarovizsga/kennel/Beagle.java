23734134134ffcdd751467437e9c0115a3519970
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2021-03-16 22:10:00 +0100

    Kutyák feladat

---
+++ src/java/hu/nive/ujratervezes/zarovizsga/kennel/Beagle.java
@@ -0,0 +1,19 @@
+package hu.nive.ujratervezes.zarovizsga.kennel;
+
+public class Beagle extends Dog{
+
+    public Beagle(String name) {
+        super(name);
+    }
+
+    @Override
+    public void feed() {
+        happiness +=2;
+    }
+
+    @Override
+    public void play(int hours) {
+        happiness = happiness + hours * 2;
+    }
+}
+
