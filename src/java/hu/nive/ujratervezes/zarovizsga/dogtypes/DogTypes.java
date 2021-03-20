7a69034c26ff52b11fb6414f26d904718ab573e0
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2021-03-20 15:58:42 +0100

    Dogtypes javított

--- src/java/hu/nive/ujratervezes/zarovizsga/dogtypes/DogTypes.java
+++ src/java/hu/nive/ujratervezes/zarovizsga/dogtypes/DogTypes.java
@@ -9,44 +9,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DogTypes {
-
     private DataSource dataSource;
 
     public DogTypes(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
+
     public List<String> getDogsByCountry(String country) {
-        try (Connection conn = dataSource.getConnection();
-
-             PreparedStatement ps = conn.prepareStatement("SELECT name FROM dog_types WHERE country = ? ORDER BY name")) {
-
-            ps.setString(1, country.toUpperCase());
-            if (getResult(ps).isEmpty()) {
-                throw new IllegalArgumentException("Not found");
-            }
-            return getResult(ps);
-
+        List<String> result = new ArrayList<>();
+        try(Connection conn = dataSource.getConnection();
+            PreparedStatement stmt = conn.prepareStatement("select `name` from dog_types where country = ? order by `name`;")) {
+            stmt.setString(1,country.toUpperCase());
+            addDogTypeToListByStatement(result, stmt);
         } catch (SQLException sqle) {
-            throw new IllegalStateException("Cannot query", sqle);
-        }
-    }
-
-    private List<String> getResult(PreparedStatement ps) {
-        try (ResultSet rs = ps.executeQuery()) {
-            return getNames(rs);
-        } catch (SQLException sqle) {
-            throw new IllegalStateException("Cannot query", sqle);
-        }
-    }
-
-
-    private List<String> getNames(ResultSet rs) throws SQLException {
-        List<String > result = new ArrayList<>();
-
-        while (rs.next()) {
-            result.add(rs.getString("name").toLowerCase());
+            throw new IllegalStateException("Cannot connect", sqle);
         }
         return result;
+    }
+
+    private void addDogTypeToListByStatement(List<String> result, PreparedStatement stmt) throws SQLException {
+        try(ResultSet rs = stmt.executeQuery()) {
+            while(rs.next()) {
+                result.add(rs.getString("name").toLowerCase());
+            }
+        }
     }
 }
